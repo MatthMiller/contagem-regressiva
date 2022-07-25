0fbe16bc13926680c594dbef0411dc686cfbf84f
Matheus Monteiro <matheus.monteiro.genesis@gmail.com>
2022-07-24 23:12:33 -0300

    remoção script de resize

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 import ContagemRegressiva from './contagem-regressiva.js';
 import MobileZoomBlock from './mobile-label-zoom-block.js';
-import CentralizarConteudo from './centralizar-ao-resize-pc.js';
 
 const contagem = new ContagemRegressiva(
     '#ano',
